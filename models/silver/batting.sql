7f31793ab3bda72d9d1f835a8b79933ee6b1c5a0
Somasundaram Sankara Narayanan <somasundaram0504@gmail.com>
2024-12-03 06:28:07 +0000

    bowling model added

--- models/silver/batting.sql
+++ models/silver/batting.sql
@@ -1,6 +1,7 @@
 {{ config(materialized='table') }}
 
-with cte as 
+
+with batting as 
 (
 select season_nm,season_rank,player_nm,group_nm,
 team_nm,matches,innings,notout,runs,fours,sixes,fifties,hundreds,
@@ -27,8 +28,9 @@ current_timestamp() as updated_utc,'dbt_svc_acct_user' as updated_by,'cric_clubs
 )
 select 
 season_nm,season_rank,player_nm,group_nm,
-team_nm,matches as match_cnt,innings as innings_cnt,notout as notout_cnt,
+team_nm,matches as bat_match_cnt,innings as bat_innings_cnt,notout as notout_cnt,
 runs as run_cnt,fours as four_cnt,sixes as six_cnt,fifties as fifty_cnt,hundreds as hundred_cnt,
-high_score,strike_rate,bat_avg as batting_avg, round(cast(strike_rate* bat_avg as numeric),2) as bat_attack_rate,
+high_score,round(strike_rate,2) as bat_strike_rate,round(bat_avg,2) as batting_avg, round(cast(strike_rate* bat_avg as numeric),2) as bat_attack_rate,
+ROUND(CAST((innings/matches) * 100 as numeric),2) as bat_opportunity_rate,
 created_utc,created_by,updated_utc,updated_by,source_nm
- from cte
+ from batting
---
+++ models/silver/bowling.sql
@@ -0,0 +1,47 @@
+with bowling_combined as 
+(
+select 
+season_nm,season_rank,player_nm,group_nm,team_nm,
+matches,innings,CAST(overs as STRING) as overs,runs,wickets,bbf,maidens,dots,economy,avg,strike_rate,hat_trick,four_wk_haul,five_wk_haul,
+wides,noballs
+,current_timestamp() as created_utc,'dbt_svc_acct_user' as created_by,
+current_timestamp() as updated_utc,'dbt_svc_acct_user' as updated_by,'cric_clubs' as source_nm
+ from `zen-cricket-club.bronze.bowling_2024_spring_1` 
+ UNION ALL
+select 
+season_nm,season_rank,player_nm,group_nm,team_nm,
+matches,innings,CAST(overs as STRING) as overs,runs,wickets,bbf,maidens,dots,economy,avg,strike_rate,hat_trick,four_wk_haul,five_wk_haul,
+wides,noballs
+,current_timestamp() as created_utc,'dbt_svc_acct_user' as created_by,
+current_timestamp() as updated_utc,'dbt_svc_acct_user' as updated_by,'cric_clubs' as source_nm
+ from `zen-cricket-club.bronze.bowling_2024_spring_2` 
+UNION ALL
+select 
+season_nm,season_rank,player_nm,group_nm,team_nm,
+matches,innings,CAST(overs as STRING) as overs,runs,wickets,bbf,maidens,dots,economy,avg,strike_rate,hat_trick,four_wk_haul,five_wk_haul,
+wides,noballs
+,current_timestamp() as created_utc,'dbt_svc_acct_user' as created_by,
+current_timestamp() as updated_utc,'dbt_svc_acct_user' as updated_by,'cric_clubs' as source_nm
+ from `zen-cricket-club.bronze.bowling_2024_spring_3` 
+)
+,
+bowling_stg as (
+select season_nm,season_rank,player_nm,group_nm,team_nm,
+matches as bowl_match_cnt,innings as bowl_innings_cnt,CAST(overs as numeric) as overs_cnt, runs as conceded_runs_cnt,
+wickets as wicket_cnt, maidens as maiden_cnt, dots as dot_ball_cnt, round(CAST(economy as numeric),2) as economy_rate,
+round(CAST(avg as numeric),2) as bowling_avg,round(CAST(strike_rate as numeric),2) as bowl_strike_rate, hat_trick as hat_trick_cnt,
+four_wk_haul as four_wkt_haul_cnt, five_wk_haul as five_wkt_haul_cnt, wides as wide_cnt, noballs as no_ball_cnt,
+ CASE WHEN STRPOS(overs,'.') > 0 THEN (CAST(LEFT(overs,STRPOS(overs,'.')-1) AS INT64) * 6)  + CAST(RIGHT(overs,1) AS INT64)
+ELSE CAST(overs as INT64) * 6
+END as ball_cnt,
+ floor(CAST(((CASE WHEN STRPOS(overs,'.') > 0 THEN CAST(LEFT(overs,STRPOS(overs,'.')-1) AS INT64)  + 1
+ELSE CAST(overs as INT64)
+END)/innings) as NUMERIC)) as min_overs_bowled_cnt,
+round(CAST((1/strike_rate) * wickets as numeric),2) as bowl_attack_rate,
+trim(bbf) as best_bowling_figure,
+created_utc,created_by,
+updated_utc,updated_by,source_nm
+from bowling_combined)
+
+select bs.*, 
+from bowling_stg bs
